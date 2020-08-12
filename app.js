35305ab44b0e45d73e73fe7ae570efdc74a02fbf
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-13 01:03:01 +0530

    refactor: restructure code base

--- app.js
+++ app.js
@@ -16,48 +16,6 @@ const express = require('express'),
 
 const port = process.env.PORT || 3000;
 
-// Set The Storage Engine
-const storage = multer.diskStorage({
-    destination: './public/images/uploaded',
-    filename: (req, file, cb) => {
-        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-
-
-// Check if the user is logged in
-const isLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login');
-}
-
-// Check File Type
-const checkFileType = (file, cb) => {
-    // Allowed ext
-    const filetypes = /jpeg|jpg|png|gif/;
-    // Check ext
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // Check mime
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (mimetype && extname) {
-        return cb(null,true);
-    } else {
-        cb('Error: Images Only!');
-    }
-}
-
-// Init Upload
-const upload = multer({
-    storage: storage,
-    limits:{fileSize: 2000000},
-    fileFilter: (req, file, cb) => {
-        checkFileType(file, cb);
-    }
-}).single('image');
-
 // APP CONFIG
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -159,6 +117,49 @@ passport.use(
                 });
         })
 );
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/images/uploaded',
+    filename: (req, file, cb) => {
+        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+
+// Check if the user is logged in
+const isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+// Check File Type
+const checkFileType = (file, cb) => {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null,true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
+// Init Upload
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 2000000},
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
+    }
+}).single('image');
+
 
 connection.query('USE blog_app');
 
