0b7d9c012faff5b827fa574a6d64b5c9db0541bf
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-15 00:37:47 +0530

    refactor: use mysql excape operators in comments

--- routes/comments.js
+++ routes/comments.js
@@ -5,17 +5,21 @@ const router = express.Router();
 
 // CREATE ROUTE - COMMENTS
 router.post('/blogs/:id', isLoggedIn, (req, res) => {
-    const blog_id = req.params.id;
-    pool.query('INSERT INTO comments(comment_text,blog_id,user_id) VALUES(?,?,?);', [req.body.comment.comment_text, blog_id, req.user.id], error => {
-        if (error) throw error;
-        res.redirect('/blogs/' + blog_id);
+    const query = 'INSERT INTO comments(comment_text,blog_id,user_id) VALUES(?,?,?);';
+    const blogId = req.params.id;
+
+    pool.query(query, [req.body.comment.comment_text, blogId, req.user.id], err => {
+        if (err) throw err;
+        res.redirect('/blogs/' + blogId);
     });
 });
 
 // DELETE ROUTE - COMMENTS
 router.delete('/blogs/:id/:comment_id', isLoggedIn, (req, res) => {
-    pool.query('DELETE FROM comments WHERE id = ' + req.params.comment_id + ';', error => {
-        if (error) throw error;
+    const query = 'DELETE FROM comments WHERE id = ?;';
+
+    pool.query(query, [req.params.comment_id], err => {
+        if (err) throw err;
         res.redirect('/blogs/' + req.params.id);
     });
 });
