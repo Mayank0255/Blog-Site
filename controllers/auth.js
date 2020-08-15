da5a3b0c46d01713493145e6d26016fe5702db89
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-16 01:00:59 +0530

    feat: auth controller setup

---
+++ controllers/auth.js
@@ -0,0 +1,103 @@
+const passport = require('passport');
+const bcrypt = require('bcrypt-nodejs');
+const LocalStrategy = require('passport-local');
+
+const express = require('express');
+const router = express.Router();
+
+// passport sign-up
+passport.use(
+    'local-signup',
+    new LocalStrategy({
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+        (req, username, password, done) => {
+            pool.query('SELECT * FROM users WHERE username = ? ', [username], (err, rows) => {
+                if (err)
+                    return done(err);
+                if (rows.length) {
+                    return done(null, false, req.flash('signupMessage', 'That is already taken'));
+                } else {
+                    const newUserMysql = {
+                        username: username,
+                        password: bcrypt.hashSync(password, null, null)
+                    };
+
+                    const insertQuery = 'INSERT INTO users (username, password) values (?, ?)';
+                    const params = [newUserMysql.username, newUserMysql.password];
+
+                    pool.query(insertQuery, params,
+                        (err, rows) => {
+                            newUserMysql.id = rows.insertId;
+
+                            return done(null, newUserMysql);
+                        });
+                }
+            });
+        })
+);
+
+// passport login
+passport.use(
+    'local-login',
+    new LocalStrategy({
+            usernameField: 'username',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+        (req, username, password, done) => {
+            pool.query('SELECT * FROM users WHERE username = ? ', [username],
+                (err, rows) => {
+                    if (err)
+                        return done(err);
+                    if (!rows.length) {
+                        return done(null, false, req.flash('loginMessage', 'No User Found'));
+                    }
+                    if (!bcrypt.compareSync(password, rows[0].password))
+                        return done(null, false, req.flash('loginMessage', 'Wrong Password'));
+
+                    return done(null, rows[0]);
+                });
+        })
+);
+
+// register user
+const registerPage = (req, res) => {
+    res.render('register');
+};
+
+ const register = (req, res) => {
+    passport.authenticate('local-signup', {
+        successRedirect: '/',
+        failureRedirect: '/register',
+        failureFlash: true
+    })(req, res);
+};
+
+// log-in user
+const loginPage = (req, res) => {
+    res.render('login');
+};
+
+const login = (req, res) => {
+    passport.authenticate('local-login', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    })(req, res);
+};
+
+const logout = (req, res) => {
+    req.logout();
+    res.redirect('/');
+};
+
+module.exports = authController = {
+    registerPage,
+    register,
+    loginPage,
+    login,
+    logout
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,97 +1,16 @@
-const passport = require('passport');
-const bcrypt = require('bcrypt-nodejs');
-const LocalStrategy = require('passport-local');
-
-const express = require('express');
-const router = express.Router();
-
-// passport sign-up
-passport.use(
-    'local-signup',
-    new LocalStrategy({
-            usernameField: 'username',
-            passwordField: 'password',
-            passReqToCallback: true
-        },
-        (req, username, password, done) => {
-            pool.query('SELECT * FROM users WHERE username = ? ', [username], (err, rows) => {
-                if (err)
-                    return done(err);
-                if (rows.length) {
-                    return done(null, false, req.flash('signupMessage', 'That is already taken'));
-                } else {
-                    const newUserMysql = {
-                        username: username,
-                        password: bcrypt.hashSync(password, null, null)
-                    };
-
-                    const insertQuery = 'INSERT INTO users (username, password) values (?, ?)';
-                    const params = [newUserMysql.username, newUserMysql.password];
-
-                    pool.query(insertQuery, params,
-                        (err, rows) => {
-                            newUserMysql.id = rows.insertId;
-
-                            return done(null, newUserMysql);
-                        });
-                }
-            });
-        })
-);
-
-// passport login
-passport.use(
-    'local-login',
-    new LocalStrategy({
-            usernameField: 'username',
-            passwordField: 'password',
-            passReqToCallback: true
-        },
-        (req, username, password, done) => {
-            pool.query('SELECT * FROM users WHERE username = ? ', [username],
-                (err, rows) => {
-                    if (err)
-                        return done(err);
-                    if (!rows.length) {
-                        return done(null, false, req.flash('loginMessage', 'No User Found'));
-                    }
-                    if (!bcrypt.compareSync(password, rows[0].password))
-                        return done(null, false, req.flash('loginMessage', 'Wrong Password'));
-
-                    return done(null, rows[0]);
-                });
-        })
-);
+const router = require('express').Router();
+const authController = require('../controllers/auth');
 
 // register user
-router.get('/register', (req, res) => {
-    res.render('register');
-});
+router.get('/register', authController.registerPage);
 
-router.post('/register', (req, res) => {
-    passport.authenticate('local-signup', {
-        successRedirect: '/',
-        failureRedirect: '/register',
-        failureFlash: true
-    })(req, res);
-});
+router.post('/register', authController.register);
 
 // log-in user
-router.get('/login', (req, res) => {
-    res.render('login');
-});
+router.get('/login', authController.loginPage);
 
-router.post('/login', (req, res) => {
-    passport.authenticate('local-login', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    })(req, res);
-});
+router.post('/login', authController.login);
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
-});
+router.get('/logout', authController.logout);
 
 module.exports = router;
